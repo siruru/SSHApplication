d31aceaf28c14488a2c1a5a2ed559c042f805beb
Kouki Yamaguchi <siruru18@gmail.com>
2016-09-25 21:50:50 +0900

    SSH 接続確認

---
+++ SSHtest/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SSHtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textboxHostname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mtextPortNumber = new System.Windows.Forms.MaskedTextBox();
+            this.ConnectionButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ホスト名";
+            // 
+            // textboxHostname
+            // 
+            this.textboxHostname.Location = new System.Drawing.Point(78, 10);
+            this.textboxHostname.Name = "textboxHostname";
+            this.textboxHostname.Size = new System.Drawing.Size(194, 19);
+            this.textboxHostname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ポート番号";
+            // 
+            // mtextPortNumber
+            // 
+            this.mtextPortNumber.Location = new System.Drawing.Point(78, 35);
+            this.mtextPortNumber.Mask = "9999";
+            this.mtextPortNumber.Name = "mtextPortNumber";
+            this.mtextPortNumber.Size = new System.Drawing.Size(74, 19);
+            this.mtextPortNumber.TabIndex = 3;
+            // 
+            // ConnectionButton
+            // 
+            this.ConnectionButton.Location = new System.Drawing.Point(12, 134);
+            this.ConnectionButton.Name = "ConnectionButton";
+            this.ConnectionButton.Size = new System.Drawing.Size(257, 23);
+            this.ConnectionButton.TabIndex = 4;
+            this.ConnectionButton.Text = "接続";
+            this.ConnectionButton.UseVisualStyleBackColor = true;
+            this.ConnectionButton.Click += new System.EventHandler(this.ConnectionButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 163);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 86);
+            this.textBox1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "秘密鍵";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 94);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(256, 19);
+            this.textBoxPath.TabIndex = 7;
+            // 
+            // buttonKey
+            // 
+            this.buttonKey.Location = new System.Drawing.Point(78, 65);
+            this.buttonKey.Name = "buttonKey";
+            this.buttonKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonKey.TabIndex = 8;
+            this.buttonKey.Text = "参照";
+            this.buttonKey.UseVisualStyleBackColor = true;
+            this.buttonKey.Click += new System.EventHandler(this.buttonKey_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonKey);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConnectionButton);
+            this.Controls.Add(this.mtextPortNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textboxHostname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SSH接続アプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textboxHostname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox mtextPortNumber;
+        private System.Windows.Forms.Button ConnectionButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonKey;
+    }
+}
+
---
+++ SSHtest/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSHtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectionButton_Click(object sender, EventArgs e)
+        {
+            //SSH接続処理
+
+        }
+
+        private void buttonKey_Click(object sender, EventArgs e)
+        {
+            string path = FileOpen();
+            textBoxPath.Text = path;
+        }
+
+        //*****************************************************//
+        //サブルーチン
+        //ファイルを開く関数
+        private string FileOpen()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();  //インスタンスを作成
+
+            //はじめのファイル名を指定
+            //はじめに[ファイル名」で表示されるファイル名
+            ofd.FileName = "id_rsa";
+
+            //はじめに表示されるフォルダ
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = "";
+
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "";
+
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //1番目の「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 1;
+
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を返す
+                return ofd.FileName;
+            }
+            else
+            {
+                //キャンセルボタンをクリックしたとき、もともと入っていたパスを保持
+                return textBoxPath.Text;
+            }
+        }
+    }
+}
